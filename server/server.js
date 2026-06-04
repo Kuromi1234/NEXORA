f48a5b60cded29c2d0dd8394e95d2b2f6f6db03c
Arjun <natharjun028@gmail.com>
2026-06-05 00:53:53 +0530

    feat: enhance server configuration and add new routes for restaurant and bookings

--- server/server.js
+++ server/server.js
@@ -1,17 +1,36 @@
-const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const dotenv = require("dotenv");
+const path = require("path");
+
+console.log("NODE_ENV =", process.env.NODE_ENV);
+
 dotenv.config({
-  path: `../.env.${process.env.NODE_ENV}`
+  path: path.join(
+    __dirname,
+    `../.env.${process.env.NODE_ENV || "development"}`
+  ),
 });
 
 const app = express();
 
-app.use(morgan());
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT || 3000;
+
+app.get("/restruant", (req, res) => {
+  res.json({restruant:["Pizza Hut", "Domino's", "KFC"]});
+});
+
+app.post("/bookings", (req,res)=>{
+  res.json({bookings:"confimed"})
+});
+
+app.post("/auth/login", (req,res)=>{
+  res.json({token:generatedToken});
+});
+
+const PORT = process.env.PORT || 2000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
